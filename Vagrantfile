a6d9f8b0bdc1fa201944512ac1c9995f54513b9c
CEnnis91 <cennis91@gmail.com>
2021-03-13 16:18:15 -0500

    First pass, get smokeping running with an empty config

---
+++ Vagrantfile
@@ -0,0 +1,35 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'time'
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "generic/alpine312"
+  config.vm.hostname = "netplay-analyzer.local"
+
+  config.vm.network "private_network", type: "dhcp", auto_config: false
+  config.vm.network "public_network", hostname: true
+
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+  config.vm.synced_folder "config", "/config", create: true
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "smash64-dev network analyzer (" + Time.now.to_i.to_s + ")"
+    vb.memory = 512
+    vb.cpus = 1
+    vb.gui = true
+  end
+
+  config.vm.provision "file", source: "provision", destination: "/tmp/"
+  config.vm.provision "shell", path: "scripts/bootstrap.sh"
+  config.vm.provision "shell", run: "always", inline: "rm /etc/localtime && ln -s " + getTimezone + " /etc/localtime"
+end
+
+def getTimezone
+  # based on https://stackoverflow.com/a/46778032
+
+  offset = ((Time.zone_offset(Time.now.zone) / 60) / 60)
+  suffix = offset >= 0 ? "-#{offset.abs.to_s}" : "+#{offset.abs.to_s}"
+
+  return '/usr/share/zoneinfo/Etc/GMT' + suffix
+end
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+BASEDIR="/tmp/provision"
+
+
+# packages
+apk add --no-cache \
+	apache-mod-fcgid \
+	apache2 \
+	apache2-ctl \
+	apache2-utils \
+	bc \
+	bind-tools \
+	curl \
+	font-noto-cjk \
+	openssh-client \
+	smokeping \
+	sudo \
+	tcptraceroute \
+	ttf-dejavu \
+	tzdata
+install -m755 -D $BASEDIR/tcpping /usr/bin/
+
+
+# directories
+mkdir -p \
+	/etc/smokeping/config.d \
+	/var/cache/smokeping
+
+
+# apache2
+cp -f $BASEDIR/apache2/smokeping.conf /etc/apache2/conf.d/
+
+
+# smokeping
+sed -i 's|//cropper/|/cropper/|' /etc/smokeping/basepage.html
+cp -f $BASEDIR/smokeping/config /etc/smokeping/
+cp -rf $BASEDIR/smokeping/config.d /etc/smokeping/
+
+ln -sf /usr/share/webapps/smokeping /var/www/localhost/smokeping
+ln -sf /var/cache/smokeping /var/www/localhost/smokeping/cache
+
+
+# permissions
+chmod a+s /usr/bin/traceroute
+chmod a+s /usr/bin/tcptraceroute
+chmod -R 777 /var/cache/smokeping && chown -R smokeping:smokeping /var/cache/smokeping
+chmod -R 777 /var/lib/smokeping/.simg
+
+
+# services
+rc-update add apache2 default && rc-service apache2 restart
+rc-update add smokeping default && rc-service smokeping restart
